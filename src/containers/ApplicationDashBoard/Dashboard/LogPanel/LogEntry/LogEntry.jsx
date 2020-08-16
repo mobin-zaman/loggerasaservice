e6594afa9b6d069b1defc7948022e89eae1673a6
mobin_zaman <mobinzamandev@gmail.com>
2020-08-16 17:41:45 +0600

    css related to log panel fixed now

--- src/containers/ApplicationDashBoard/Dashboard/LogPanel/LogEntry/LogEntry.jsx
+++ src/containers/ApplicationDashBoard/Dashboard/LogPanel/LogEntry/LogEntry.jsx
@@ -5,10 +5,10 @@ import style from './style.module.css';
 const LogEntry = ({type, description, created_at}) =>  {
 
     return (
-    <div className={style.div_style}>
+    <div>
         <span className={style.span_style}>{created_at}</span>
         <span className={style.span_type}>{type}</span>
-        <span className={style.span_style}>{description}</span>
+        <span className={style.span_description}>{description}</span>
     </div>
     );
 }
