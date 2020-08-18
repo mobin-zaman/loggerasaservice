a7bebb78747d982c9472976c173efe842e7dd59d
mobin_zaman <mobinzamandev@gmail.com>
2020-08-18 16:57:42 +0600

    table is finalized for the time being

--- src/containers/ApplicationDashBoard/Dashboard/LogPanel/LogPanel.jsx
+++ src/containers/ApplicationDashBoard/Dashboard/LogPanel/LogPanel.jsx
@@ -1,41 +1,34 @@
 import React from 'react';
-import LogEntry from "./LogEntry/LogEntry";
-import { Table, Column } from "react-virtualized";
+import {Table, Column } from "react-virtualized";
 import 'react-virtualized/styles.css'; // only needs to be imported once
-import shortid from 'shortid';
-// const LogPanel = ({logs}) => {
-//    return(
-//    <div>
-//       {logs.map((d)=> <LogEntry key={shortid.generate()} type={d.type} description={d.description} created_at={d.created_at}/>)}
-//    </div>
-//    )
-// }
+import style from "./LogEntry/style.module.css";
+
 const LogPanel = ({logs}) => {
     const listHeight = 1000;
     const rowHeight = 100;
     const rowWidth = 850;
+   const useDynamicRowHeight = true;
 
 
-const renderLogs = ({index,key,style}) => (
-       <LogEntry key={key} style={style} type={logs[index].type} description={logs[index].description} created_at={logs[index].created_at}/>
-
-)
-
-
+const renderLogs = ({cellData}) => (
+        <span className={style.span_description}>{cellData.replace(/\\n/g, '\n')}</span>
+    )
 
 
 
     return (
         <Table
             width={1350}
-            height={800}
+            height={700}
             headerHeight={20}
             rowHeight={30}
             rowCount={logs.length}
-            rowGetter={({index})=>logs[index]}>
-            <Column label="timestamp" dataKey="created_at" width={200}/>
+            rowGetter={({index})=>logs[index]}
+        >
+
+            <Column label="timestamp" dataKey="created_at" width={150}/>
             <Column label="type" dataKey="type" width={100} />
-            <Column width={200} label="log" dataKey="description"/>
+            <Column width={1000} className={style.span_description} label="log" dataKey="description" cellRenderer={(cellData)=> renderLogs(cellData)}/>
         </Table>
 
     )
