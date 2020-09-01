690b1dc4dcd9390f27b07f3aa5fc355503d82b22
mobin_zaman <mobinzamandev@gmail.com>
2020-09-01 18:55:49 +0600

    log panel finalized

--- src/containers/ApplicationDashBoard/Dashboard/LogPanel/LogEntry/LogModal.jsx
+++ src/containers/ApplicationDashBoard/Dashboard/LogPanel/LogEntry/LogModal.jsx
@@ -13,6 +13,18 @@ const LogModal = ({log, isOpen, onCancel}) =>  {
             backgroundColor: '#181A1B'
         }
     }
+    const RenderLogTypes = ({logType}) => {
+        switch (logType) {
+            case "ERROR":
+                return <div className={style.log_type_error}>{logType}</div>
+            case "WARN":
+                return <div className={style.log_type_warn}>{logType}</div>
+            case "INFO":
+                return <div className={style.log_type_info}>{logType}</div>
+            default:
+                return <div className={style.log_type_others}>{logType}</div>
+        }
+    }
 
 
     return (
@@ -20,7 +32,7 @@ const LogModal = ({log, isOpen, onCancel}) =>  {
                        onRequestClose={onCancel}>
 
                     <span className={style.modal_span}>TIMESTAMP</span><pre className={style.pre_time_stamp}>{log.created_at}</pre>
-                    <span className={style.modal_span}>TYPE</span><pre className={style.pre_type}>{log.type}</pre>
+                    <span className={style.modal_span}>TYPE</span><pre className={style.pre_type}><RenderLogTypes logType={log.type}/> </pre>
                     <span className={style.modal_span}>LOG</span><pre className={style.pre_log}>{log.description}</pre>
                     <button className={style.modal_button} type ="button" onClick={onCancel}>Back</button>
                 </Modal>
